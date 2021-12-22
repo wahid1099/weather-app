93c67dab76d2217ea66e7b98a696be772e4922e5
Abdul Wahid <wahdiahmed890@gmail.com>
2021-12-22 22:16:51 +0600

    adding

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
+
+import React, { useState, useEffect } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,ActivityIndicator } from 'react-native';
+
+import Weather from './Components/Weather';
+import SearchBar from './Components/SearchBar';
+const API_KEY = "46a9246bebba16d42b36aac3fc3ba8af";
 
 export default function App() {
+  const [weatherData, setWeatherData] = useState(null);
+    const [loaded, setLoaded] = useState(true);
+
+    async function fetchWeatherData(cityName) {
+      setLoaded(false);
+      const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`
+      try {
+          const response = await fetch(API);
+          if(response.status == 200) {
+              const data = await response.json();
+              setWeatherData(data);
+              console.log(data);
+          } else {
+              setWeatherData(null);
+          }
+          setLoaded(true);
+          
+      } catch (error) {
+          console.log(error);
+      }
+  }
+//   useEffect(() => {
+//    fetchWeatherData();
+// }, [])
+
+
+if(!loaded) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View style={styles.container}>
+          <ActivityIndicator color='gray'  size={36} />
+      </View>
+
+  )
+}
+
+else if(weatherData === null) {
+  return (
+      <View style={styles.container}>
+          <SearchBar fetchWeatherData={fetchWeatherData}/>
+          <Text style={styles.primaryText}>City Not Found! Try Different City</Text>
+      </View>
+  )
+}
+
+return (
+  <View style={styles.container}>
+      <Weather weatherData={weatherData} fetchWeatherData={fetchWeatherData}  />
+  </View>
+);
+
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +68,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  primaryText: {
+      margin: 20,
+      fontSize: 28
+  }
 });
